1cf4c5eb1980a8b8116aa137aad35dc19bc0ca78
Kashyap Mayank <112820100+mayankified@users.noreply.github.com>
2024-07-21 11:45:26 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -24,7 +24,7 @@ const App = () => {
         <Route path='/signup' element={<Signup />} />
        
 
-        <Route path='/changepassword' element={<ForgotPassword />} />
+        <Route path='/forgotpassword' element={<ForgotPassword />} />
         <Route path="/login" element={<Login />} />
         <Route path='/signup' element={<Signup />} />
         <Route path='/products/:cat' element={<Catpage/>} />
